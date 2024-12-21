c34b08ca665a4ba7d9728744abea05e07dbcceeb
冯思程 <fscdyx888@163.com>
2024-12-21 13:54:05 +0800

    fix(user)

--- histogram-node/src/main.js
+++ histogram-node/src/main.js
@@ -1,14 +1,6 @@
-import * as d3 from "d3";
-import { renderChart } from "./user.js";
 import steam from "./steam.js";
-import csv from "./assets/chart.csv";
+import renderChart from "./user.js";
 
 steam();
 
-document.addEventListener("DOMContentLoaded", () => {
-    d3.csv(csv).then(data => {//
-        renderChart("chart", data);
-    }
-    );
-
-});
+renderChart();
